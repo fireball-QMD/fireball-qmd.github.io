1df9165a5aeb54d6a7abe374ce17927161da6809
fireball-QMD <elius.jom@gmail.com>
2017-04-20 12:34:28 +0200

    init

---
+++ in_element.sh
@@ -0,0 +1,49 @@
+
+echo '<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<TITLE>'  > index.html
+
+echo $1 >> index.html
+
+echo '</TITLE>
+<LINK REL="STYLESHEET" HREF="../css">
+</HEAD>
+
+<BODY>
+' >> index.html
+
+for i in *tar.gz 
+do 
+tar -xvf $i
+echo '<H1><A HREF="'$i  >> index.html
+echo '">' >> index.html
+echo ${i::-7} >> index.html
+echo '</A>
+</H1>
+<ul>
+' >> index.html
+
+n=$(wc -l  ${i::-7}/info | cut -d' ' -f1)
+for((j=1;j<=$n;j++))
+do 
+l=$(head -$j ${i::-7}/info | tail -1)
+if [ ${#l} -gt 0 ]
+then
+ echo '<li>' $l' </li>' >>  index.html
+fi
+done
+
+rm -fr ${i::-7}
+
+if test -d  ${i::-7}.more
+then
+echo '<li><A HREF="'${i::-7}'.more/index.html' >> index.html
+echo '">read more</A></li>' >> index.html
+
+fi
+echo '</ul>'>>index.html
+done 
+
+
+# i=H_02.tar.gz ; tar -xvf $i ; vi ${i::-7}/info ; rm -fr $i ; tar -cf ${i::-7}.tar ${i::-7} ; gzip ${i::-7}.tar ; rm -fr ${i::-7}
