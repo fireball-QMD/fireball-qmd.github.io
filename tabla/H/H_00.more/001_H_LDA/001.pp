1a61a4b3d6bc7fdeb15a1f168842e8efebd91b45
dgtrabada <dgtrabada@gmail.com>
2024-11-24 09:43:55 +0000

    H/H00.more

---
+++ tabla/H/H_00.more/001_H_LDA/001.pp
@@ -0,0 +1,39 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#info ./psgen ncpp 00:06:15 feb 17 2022 dani                          
+                                                                      
+fhi pseudopotential tool fhipp - version rev270603B                   
+                                                                      
+               chemical symbol   H                                  
+                nuclear charge   1.00                                 
+                  total charge   0.00                                 
+         number of core states   0                                    
+      number of valence states   1                                    
+    exchange-correlation model   3      LDA CA Perdew/Zunger 1980 
+      scalar-relativistic mode                                        
+        parameters radial mesh   387    1.024700  0.625000E-02        
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    3   0.00                            ! iexc , exmix
+    1                                   ! nshells
+    0                                   ! L values
+    1.00000                             ! Z val
+   50.00000                             ! alpha
+    0.20000                             ! Rcutoff
+           5
+  0.0000000E+00  0.0000000E+00
+  4.9999999E-03  0.0000000E+00
+  9.9999998E-03  0.0000000E+00
+  1.5000000E-02  0.0000000E+00
+  2.0000000E-02  0.0000000E+00
+ L=0 Npoint=    5
+  0.0000000E+00  0.0000000E+00
+  4.9999999E-03  0.0000000E+00
+  9.9999998E-03  0.0000000E+00
+  1.5000000E-02  0.0000000E+00
+  2.0000000E-02  0.0000000E+00
+ L=0 Npoint=    5 cl=     0.0000000  
+  0.0000000E+00  0.0000000E+00
+  4.9999999E-03  0.0000000E+00
+  9.9999998E-03  0.0000000E+00
+  1.5000000E-02  0.0000000E+00
+  2.0000000E-02  0.0000000E+00
+ 
